dbd249acdf629698a7b167246a55ea451b6db2a6
Kartikeya Saxena <kasaxena@microsoft.com>
2023-04-09 18:52:34 +0530

    Added Player Controller and Spike Controllers

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Configs")]
+    [SerializeField] private float speed;
+    [SerializeField] private float radius;
+
+    private readonly Vector3 pivotAxis = new(0, 0, -1);
+    private readonly Vector3 pivotPoint = new(0, 0, 0);
+
+    void Start()
+    {
+        transform.position = new(radius, 0, 0);
+    }
+
+    void Update()
+    {
+        transform.RotateAround(pivotPoint, pivotAxis, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SpikeSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeSpawner : MonoBehaviour
+{
+    [Header("Prefabs")]
+    [SerializeField] private GameObject spike;
+
+    [Header("Configs")]
+    [SerializeField] private float spawnRadius;
+    [SerializeField] private float maxSpawnWaitTime;
+    [SerializeField] private float minSpawnWaitTime;
+
+
+    void Start()
+    {
+        StartCoroutine(SpawnSpikes());
+    }
+
+    IEnumerator SpawnSpikes()
+    {
+        while(true)
+        {
+            float spawnWaitTime = Random.Range(minSpawnWaitTime, maxSpawnWaitTime);
+            float angle = Random.Range(0f, 360f);
+            Vector3 position = new(spawnRadius * Mathf.Cos(angle), spawnRadius * Mathf.Sin(angle), 0);
+            Instantiate(spike, position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnWaitTime);
+        }
+    }
+
+}
